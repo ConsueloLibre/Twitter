b1fdca983228f96d72781c044dcf912ad6108b85
ConsueloLibre <consuelomariana.acevedo@gmail.com>
2017-11-14 17:40:27 -0300

    suebiendo, luego de fail

---
+++ README.md
@@ -0,0 +1,21 @@
+*Twuiter*
+**Comenzando de la poca sabiduria**
+Al comenzar todo me base en el modelo replicado en una clase de Belén, siendo esta Instagram,
+fue bien dificil, pero paso a paso logre relizar un modelo bastante basico de dinamismo en JS.
+
+**Agregando dinamismo**
+Gracias a la guia de varias compañeras logre darle textura los siguientes elemento.
+
+-Boton de "twitear" desabilitado.  
+cuando no existe ningun caracter en mi textarea, y habilitando este cuando minimo se ingresa un caracter, esto en base a una función.
+
+-Ademas inserte un contador, que va en decrecendo cada vez que el espacio del textarea es utilizado por un caracter.
+
+-Textarea se aranda al ir creciendo el contenido en su interior.
+
+**Lo que mas mem gusto**
+Darle estilo a traves de Css, (hermoso), me gustaa aprender cosas nuevas inspectandolo constantemente.
+
+**Lo quemenos me gusto**
+Perder bastante tiempo cabezonenadome para arreglar el div nuevo que aparece luego de ingresar el texto y twitear,
+el div creado con DOM, no puede darle el estilo que deseaba, mucho tiempo y no logrado.
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,11 @@
 function add (){
   var comments= document.getElementById('comment').value;// con esto estamos incluyendo el valor de ingreso dentro de nuestra textarea
-
+  var comment=document.getElementById('comment');
 
   var cont= document.getElementById('cont');
 
   document.getElementById('comment').value = ""; // con esto se limpia el cuadro de texto para volver a escribir//
   var NewComments= document.createElement('div');// aca se almacena los textos//
-
 
   var paragraph= document.createElement('p');
   var nodoText=document.createTextNode(comments);
@@ -26,9 +25,27 @@ function disab(element){
   }else {
     button.disabled = false;
   }
-  var valor=document.getElementById('conteo');
-  var max=140;
+  var valor=document.getElementById('conteo');//creando el contador
+  var max=140;//el contadr empieza en
   var text=document.getElementById('comment').value;
-  var long=text.length;
-  valor.innerHTML=max-long;
+  var long=text.length;// se asocia el contador versus el largo de las letras
+  valor.innerHTML=max-long;//con esto ingresamos al HTML para modificar el contenido de el elememnto p que congtiene el nuemro del contador
+  autosize(element);
+}
+function autosize (el){//esta funcion sire para que mi caja de texto crezca en base a los caracteres ingresados
+  setTimeout(function(){
+    el.style.cssText= 'heigth:auto;padding:0';
+    el.style.cssText='heigth:'+ el.scrollHeight + 'px';
+  },0);
+}
+var textarea = document.querySelector('textarea');
+
+textarea.addEventListener('keydown', autosize);
+
+function autosize(){
+  var el = this;
+  setTimeout(function(){
+    el.style.cssText = 'height:auto; padding:0';
+    el.style.cssText = 'height:' + el.scrollHeight + 'px';
+  },0);
 }
